3dc049950faef8a6e2baee68a0e87f089e9520a4
Wellington Menezes <ligton.ribeiro@gmail.com>
2021-06-17 23:08:19 -0300

    chore: add support dynamic content

--- receitas/views.py
+++ receitas/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+receitas = {
+    1: 'Sopa de Carne',
+    2: 'Sorvete',
+    3: 'Sopa de Legumes',
+    4: 'Lasanha'
+}
+
+dados = {
+    'nome_das_receitas': receitas
+}
+
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', dados)
 
 
 def receita(request):
